339e7cbbdbb6d64c5b96cd0410c97f6eb22f6f2e
almagar <81564156+almagar@users.noreply.github.com>
2022-11-04 22:44:05 +0100

    Update pom.xml

--- backend/src/main/java/com/example/socialnetwork/SocialNetworkApplication.java
+++ backend/src/main/java/com/example/socialnetwork/SocialNetworkApplication.java
@@ -2,8 +2,10 @@ package com.example.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// remove exclude when connect to db
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SocialNetworkApplication {
 
 	public static void main(String[] args) {
