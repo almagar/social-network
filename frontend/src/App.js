82311806e67e1c1e1f1f4731feef24e413d4aeb0
linus345 <linusekman01@gmail.com>
2022-11-08 18:22:37 +0100

    keycloak configuration for react frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,27 @@
 
 function App() {
+  const getResource = async (url) => {
+    const result = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem("sn-token")}`,
+        "Content-Type": "application/json"
+      },
+      redirect: "follow"
+    })
+    console.log("result:")
+    console.log(result.json())
+  }
+
   return (
     <div className="App">
       <p>social-network:)</p>
+      <button
+        onClick={() => getResource("http://localhost:8080/open")}
+      >Open</button>
+      <button
+        onClick={() => getResource("http://localhost:8080/user")}
+      >Closed</button>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,60 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Keycloak from 'keycloak-js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let initOptions = {
+  url: "http://localhost:8282/auth",
+  redirectUri: "http://localhost:3000/",
+  realm: "socialnetwork",
+  clientId: "socialnetworkclient",
+  onLoad: "login-required",
+  checkLoginIframe: false
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const keycloak = new Keycloak(initOptions);
+
+keycloak.init({
+    onLoad: initOptions.onLoad,
+    checkLoginIframe: initOptions.checkLoginIframe,
+    redirectUri: initOptions.redirectUri
+  })
+  .then(auth => {
+
+    if (!auth) {
+      window.location.reload();
+    } else {
+      console.info("Authenticated");
+    }
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+
+    localStorage.setItem("sn-token", keycloak.token);
+    localStorage.setItem("sn-refresh-token", keycloak.refreshToken);
+
+    setTimeout(() => {
+      keycloak.updateToken(70).then(refreshed => {
+        if (refreshed) {
+          console.debug("Token refreshed" + refreshed);
+        } else {
+          console.warn("Token not refreshed, valid for "
+            + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + " seconds");
+        }
+      }).catch(() => {
+        console.error("Failed to refresh token");
+      });
+    }, 60000)
+
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    reportWebVitals();
+}).catch(() => {
+  console.error("Keycloak authentication failed")
+});
