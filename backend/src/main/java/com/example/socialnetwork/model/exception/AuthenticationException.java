f8e077f664125ae0d6e165cf344ae36a4a839b4f
almagar <81564156+almagar@users.noreply.github.com>
2022-11-14 19:40:47 +0100

    Add AbstractService and exceptions

---
+++ backend/src/main/java/com/example/socialnetwork/model/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.example.socialnetwork.model.exception;
+
+/**
+ * Exception that is thrown when an authentication error occurs.
+ */
+public class AuthenticationException extends RuntimeException {
+}
--- backend/src/main/java/com/example/socialnetwork/response/Error.java
+++ backend/src/main/java/com/example/socialnetwork/response/Error.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
  * Enumeration of errors to be used in responses.
  */
 public enum Error {
+    AUTHENTICATION_ERROR(HttpStatus.FORBIDDEN, "An authentication error has occurred"),
+    REQUIRED_FIELDS(HttpStatus.BAD_REQUEST, "Required fields are missing"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found");
 
     private final HttpStatus status;
