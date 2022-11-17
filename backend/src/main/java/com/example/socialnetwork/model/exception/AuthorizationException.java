c6e6e8eab135eec1b267f8cb3d8850660eb0588d
linus345 <linusekman01@gmail.com>
2022-11-18 00:43:56 +0100

    backend: added AuthorizationException and types to the Error enum

---
+++ backend/src/main/java/com/example/socialnetwork/model/exception/AuthorizationException.java
@@ -0,0 +1,7 @@
+package com.example.socialnetwork.model.exception;
+
+/**
+ * Exception that is thrown when an authorization error occurs.
+ */
+public class AuthorizationException extends RuntimeException {
+}
