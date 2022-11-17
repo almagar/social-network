bad25bcf23a44f8c2f56f56e366d5fc7764e9831
linus345 <linusekman01@gmail.com>
2022-11-18 00:44:57 +0100

    backend: added DAO for ChatRoom

--- backend/src/main/java/com/example/socialnetwork/dao/ChatRoomDAO.java
+++ backend/src/main/java/com/example/socialnetwork/dao/ChatRoomDAO.java
@@ -1,11 +1,18 @@
 package com.example.socialnetwork.dao;
 
+import com.example.socialnetwork.model.User;
 import com.example.socialnetwork.model.chat.ChatRoom;
+import org.springframework.data.util.Streamable;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
  * Data access object for the {@link ChatRoom} entity.
  */
 public interface ChatRoomDAO extends GenericDAO<ChatRoom, UUID> {
+    Optional<ChatRoom> findByName(String name);
+    Streamable<ChatRoom> findByOwnerOrderByName(User user);
+    Streamable<ChatRoom> findByUsersContainingOrderByName(User user);
 }
