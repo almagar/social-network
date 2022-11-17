389f10ae1c500fd36ea4bc9207f58634f7390010
linus345 <linusekman01@gmail.com>
2022-11-18 00:43:56 +0100

    backend: added AuthorizationException and types to the Error enum

--- backend/src/main/java/com/example/socialnetwork/response/Error.java
+++ backend/src/main/java/com/example/socialnetwork/response/Error.java
@@ -7,11 +7,18 @@ import org.springframework.http.HttpStatus;
  */
 public enum Error {
     AUTHENTICATION_ERROR(HttpStatus.FORBIDDEN, "An authentication error has occurred"),
+
+    AUTHORIZATION_ERROR(HttpStatus.FORBIDDEN, "Operation not allowed"),
+
     REQUIRED_FIELDS(HttpStatus.BAD_REQUEST, "Required fields are missing"),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "Resource not found"),
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
+
+    CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "Chatroom not found"),
+
     ALREADY_FOLLOWING(HttpStatus.BAD_REQUEST, "Already following this user"),
+
     NOT_FOLLOWING(HttpStatus.BAD_REQUEST, "Not following this user"),
 
     INVALID_CONTENT_TYPE(HttpStatus.BAD_REQUEST, "Content type must be image/png or image/jpeg");
